1e810486a7f8d5951e8aea459ba128abb616d090
Pablo Bernal Lazcano <pablobernal12679@gmail.com>
2020-10-07 07:42:06 -0500

    cambio

--- PruebaP.java
+++ PruebaP.java
@@ -15,7 +15,8 @@ public class PruebaP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Prueba uno");
+        System.out.println("Prueba dos");
     }
     
 }
