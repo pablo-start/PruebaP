5ad99fe6ddf5b5a88c37f11e7c70bc1a94a86f68
Pablo Bernal Lazcano <pablobernal12679@gmail.com>
2020-10-07 07:43:17 -0500

    cambio3

--- PruebaP.java
+++ PruebaP.java
@@ -17,6 +17,7 @@ public class PruebaP {
     public static void main(String[] args) {
         System.out.println("Prueba uno");
         System.out.println("Prueba dos");
+        System.out.println("Prueba 3");
     }
     
 }
